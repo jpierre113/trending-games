65ece5f13213290289d391279cbdd6ba8b3912c2
Janna Pierre <gojanna@aol.com>
2022-08-31 20:03:29 -0400

    Started code to call API. Still debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const listTwitchGames = {
+    method: 'GET',
+    url: 'https://twitch-game-popularity.p.rapidapi.com/games',
+    headers: {
+      'X-RapidAPI-Key': '642de38df0msh16c47a2b6d94672p1ead6fjsnd5cdeb6bfb68',
+      'X-RapidAPI-Host': 'twitch-game-popularity.p.rapidapi.com'
+    }
+  };
+
+  axios.request(listTwitchGames).then(function (response) {
+  	console.log(response.data);
+  }).catch(function (error) {
+  	console.error(error);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="siteTitle">Twitch Hub</h1>
+      {
+        listTwitchGames.map(({game, index}: {game: (param: any) => void; index: number}) => {
+          return(
+            {[(game(10))]}
+            key={index}
+          );
+        })
+      }
     </div>
   );
 }
